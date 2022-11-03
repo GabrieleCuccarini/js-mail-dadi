cf7a564624af225b4442dd69a6623704e2a0b21f
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-11-03 16:04:02 +0100

    Esercizio 1 Completed

---
+++ js/dadi.js
@@ -0,0 +1,36 @@
+const userMail = prompt("Inserisci la tua email")
+
+
+// LISTA MAIL                   0                       1                           2                           3
+const listaMail = [ "andreacognome@gmail.com", "carlocognome@gmail.com", "dantecognome@gmail.com", "federicocognome@gmail.com"]
+console.log(listaMail);
+// AGGIUNGO NOMI ALLA LISTA   4                           5
+listaMail.push ("giacomocognome@gmail.com" , "marcocognome@gmail.com")
+console.log(listaMail);
+
+
+// for ( let i = 0; i < listaMail.length; i++ ) {
+    // salvo in una variabile il valore di listaSpesa[i]
+    // solo per comondità. Usare listaSpesa[i] o elementoLista è la stessa cosa
+
+    if ( userMail === "andreacognome@gmail.com" ) {
+    alert( "Congratulazioni " + userMail + ", puoi accedere al sito!" );
+}
+    else if ( userMail === "carlocognome@gmail.com" ) {
+        alert( "Congratulazioni " + userMail + ", puoi accedere al sito!" );
+}
+    else if ( userMail === "dantecognome@gmail.com" ) {
+        alert( "Congratulazioni " + userMail + ", puoi accedere al sito!" );
+}    
+    else if ( userMail === "federicocognome@gmail.com" ) {
+        alert( "Congratulazioni " + userMail + ", puoi accedere al sito!" );
+}
+    else if ( userMail === "giacomocognome@gmail.com" ) {
+        alert( "Congratulazioni " + userMail + ", puoi accedere al sito!" );
+    }
+    else if ( userMail === "marcocognome@gmail.com" ) {
+        alert( "Congratulazioni " + userMail + ", puoi accedere al sito!" );
+    }
+    else {
+        alert ("La tua mail non è valida o non è autorizzata ad accedere al sito!")
+    }
